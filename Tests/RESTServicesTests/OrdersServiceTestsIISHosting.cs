77b9f6422b6ff3c43cb61223a1b6bacac9cafb3d
Roman Arekeev <roman_arekeev@epam.com>
2015-11-30 23:05:08 +0400

    Added new .svc file for REST Orders Sevice.

--- Tests/RESTServicesTests/OrdersServiceTestsIISHosting.cs
+++ Tests/RESTServicesTests/OrdersServiceTestsIISHosting.cs
@@ -5,7 +5,7 @@
     [TestClass]
     public class OrdersServiceTestsIISHosting : BaseRestOrdersServiceTests
     {
-        private const string OrdersServiceBaseAddressIIS = "http://localhost/NorthwindWCFServices/OrdersService.svc/rest";
+        private const string OrdersServiceBaseAddressIIS = "http://localhost/NorthwindWCFServices/OrdersServiceREST.svc/";
 
         [TestMethod]
         public void GetAllTest()
