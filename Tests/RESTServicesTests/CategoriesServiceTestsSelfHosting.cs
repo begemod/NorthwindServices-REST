bcb55c156c64087cf21eaa535855d0c39a563691
Roman Arekeev <roman_arekeev@epam.com>
2015-12-14 16:10:06 +0400

    Added tests for self hosted RESTCategoriesService.

---
+++ Tests/RESTServicesTests/CategoriesServiceTestsSelfHosting.cs
@@ -0,0 +1,19 @@
+﻿namespace Tests.RESTServicesTests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class CategoriesServiceTestsSelfHosting : BaseRESTCategoriesServiceTests
+    {
+        public CategoriesServiceTestsSelfHosting()
+        {
+            this.BaseServiceAddress = "http://epruizhw0228:8733/Design_Time_Addresses/NorthwindWCFServices/CategoriesService/rest";
+        }
+
+        [TestMethod]
+        public void GetCategoryNamesTest()
+        {
+            this.BaseGetCategoryNamesTest();
+        }
+    }
+}
